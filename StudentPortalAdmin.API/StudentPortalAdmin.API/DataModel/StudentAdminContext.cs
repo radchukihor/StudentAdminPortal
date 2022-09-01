e7ffab46b68c49252b5823696903f10dac1c8f1d
Ihor Radchuk <90492848+radchukihor@users.noreply.github.com>
2022-09-01 13:51:06 +0800

    added migration

--- StudentPortalAdmin.API/StudentPortalAdmin.API/DataModel/StudentAdminContext.cs
+++ StudentPortalAdmin.API/StudentPortalAdmin.API/DataModel/StudentAdminContext.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
 namespace StudentPortalAdmin.API.DataModel
 {
-    public class StudentAdminContext
+    public class StudentAdminContext : DbContext
     {
-        public StudentAdminContext()
+        public StudentAdminContext(DbContextOptions<StudentAdminContext> options) : base(options)
         {
         }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Address> Addresses { get; set; }
     }
 }
 

39dda295ca186abe2bd509ada6e16fb6da6e8df9
Ihor Radchuk <90492848+radchukihor@users.noreply.github.com>
2022-09-03 17:00:03 +0800

    Added repositories folder

---
+++ StudentPortalAdmin.API/StudentPortalAdmin.API/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using StudentPortalAdmin.API.DataModel;
+
+namespace StudentPortalAdmin.API.Repositories
+{
+    public interface IStudentRepository
+    {
+        List<Student> GetStudents();
+    }
+}
+
---
+++ StudentPortalAdmin.API/StudentPortalAdmin.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using StudentPortalAdmin.API.DataModel;
+
+namespace StudentPortalAdmin.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext _context;
+
+        public SqlStudentRepository(StudentAdminContext context)
+        {
+            _context = context;
+        }
+        public List<Student> GetStudents()
+        {
+            return _context.Student.ToList();
+        }
+    }
+}
+
